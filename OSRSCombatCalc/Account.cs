5e5f0c54f6d41bca0d07dcd17b0d33bb7f38f73a
Connor <Woodconnor13@gmail.com>
2022-07-16 13:14:42 -0400

    1st commit - laying things out/planning

---
+++ OSRSCombatCalc/Account.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OSRSCombatCalc
+{
+    internal class Account
+    {
+        //properties
+        public int attackLevel { get; set; }
+        public int strengthLevel { get; set; }
+        public int defenceLevel { get; set; }
+        public int rangedLevel { get; set; }
+        public int prayerLevel { get; set; }
+        public int magicLevel { get; set; }
+        public int hpLevel { get; set; }
+        //constructor
+        public Account(int _attackLevel, int _strengthLevel, int _defenceLevel, int _rangedLevel, int _prayerLevel, int _magicLevel, int _hpLevel)
+        {
+            attackLevel = _attackLevel;
+            strengthLevel = _strengthLevel;
+            defenceLevel = _defenceLevel;
+            rangedLevel = _rangedLevel;
+            prayerLevel = _prayerLevel;
+            magicLevel = _magicLevel;
+            hpLevel = _hpLevel;
+        }
+    }
+}
---
+++ OSRSCombatCalc/Program.cs
@@ -0,0 +1,4 @@
+﻿using OSRSCombatCalc;
+//OSRS Combat Calc
+
+Account botPotNot = new Account(50, 76, 42, 81, 44, 93, 75);
